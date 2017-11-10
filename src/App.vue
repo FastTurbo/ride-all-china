c8d4eead7277c3595709c26be8e1d3ec21bf6f3c
FastTurbo <tpf1220@sina.com>
2017-11-10 19:07:30 +0800

    Build header

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,16 @@
             <p class="title">骑行中国</p>
             <p class="url">www.ridechina.com</p>
           </el-col>
-          <el-col :offset="4" :span="16">
-            新闻
+
+          <el-col class="news" :offset="4" :span="16">
+            <ul>
+              <li class="new">一个香港妹纸、一个台湾仔、一个广东人的环华之旅</li>
+              <li class="new">单人骑行日本九州 四国 本州地区</li>
+              <li class="new">哥俩好，骑行东南亚4国—滇藏—中尼公路—反骑新藏.</li>
+              <li class="new">愿你我永远年轻，永远热泪盈眶。----环华流浪记</li>
+              <li class="new">17-04-20 要有最朴素的生活， 与最遥远的梦想 </li>
+              <li class="new">记录一下17年暑假川进青出骑行之旅我有故事 你 来听吗</li>
+            </ul>
           </el-col>
 
 
@@ -25,7 +33,6 @@
           <el-menu-item class="nav-item" index="5">骑行装备</el-menu-item>
         </el-menu>
       </el-header>
-
       <el-main class="main">
         <router-view></router-view>
       </el-main>
@@ -55,7 +62,7 @@
 
 <style lang="less" scoped>
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: 'Microsoft YaHei', Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
@@ -63,8 +70,7 @@
 
     .header {
       height:160px !important;
-      line-height:160px;
-      background:#999;
+      padding:0;
       position: fixed;
       width: 100%;
       top: 0;
@@ -72,9 +78,11 @@
 
       .row{
         height:100px;
+
         .logo{
           font-family: "Microsoft YaHei";
-          padding-top:20px;
+          padding-top:10px;
+          color:#409EFF;
 
           p{
             margin:0;
@@ -82,25 +90,55 @@
 
           .title {
             line-height: 60px;
-            color: #fff;
             font-size: 40px;
           }
           .url{
             height:20px;
             line-height:20px;
-            color:#fff;
             font-size:16px;
           }
+        }
+
+        .news{
+          height:100px;
+          text-align:left;
+          padding:10px 20px;
+
+          ul{
+            margin:0;
+            list-style:none;
+          }
+
+          .new{
+            width:50%;
+            height:26px;
+            line-height:26px;
+            color:#999;
+            float:left;
+            cursor:pointer;
+
+            &:hover{
+              color:#409EFF;
+            }
+          }
+
         }
 
       }
 
       .nav{
         height:60px;
+        background:#409eff;
 
         .nav-item{
           width:240px;
-          font-size:16px;
+          color:#fff;
+          font-size:20px;
+
+          &:hover,&.is-active{
+            background:#a55555;
+            border-bottom:2px solid #a55555;
+          }
         }
       }
     }
